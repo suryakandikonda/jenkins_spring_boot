af76920946841df7217317676bd240ed0c1f6784
Surya Kandikonda <suryasaikandikonda@gmail.com>
2024-10-23 20:27:29 +0530

    Initial commit

---
+++ src/test/java/com/jenkinspractice/jenkinspractice/JenkinspracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinspractice.jenkinspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinspracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
